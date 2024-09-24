a36800d08b42de1f8b738afd51a0492b4491e1ad
Lawrancepious <126081719+Lawrancepious@users.noreply.github.com>
2024-09-24 15:33:37 +0530

    Add files via upload

--- ocr.py
+++ ocr.py
@@ -5,6 +5,8 @@ import json
 
 def extract_text_from_image(image):
     # Extract text from the image using pytesseract
-
+    pytesseract.pytesseract.tesseract_cmd = (
+        r"C:\Users\THENIS\ocr_webapp_env\tesseract.exe"
+    )
     text = pytesseract.image_to_string(image, lang="eng")
     return {"extracted_text": text}
